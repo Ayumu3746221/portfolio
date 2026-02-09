b4044c364098a05d38b57f99b48cad0d34f55747
Ayumu3746221 <a.kkt0307@gmail.com>
2026-02-09 12:26:28 +0900

    [fix]cloudflareの環境に適していてないライブラリを削除

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,12 @@
 import { defineConfig, envField } from "astro/config";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
-import partytown from "@astrojs/partytown";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   site: "https://ayumu3746221.dev",
   output: "static",
-  integrations: [
-    react(),
-    sitemap(),
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
-  ],
+  integrations: [react(), sitemap()],
   env: {
     schema: {
       PUBLIC_GA_MEASUREMENT_ID: envField.string({
